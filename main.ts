189c8bf791b645d2adfed9cd2e3b1afa5cf7ccbb
Proudust <proudust@gmail.com>
2022-09-11 03:00:02 +0900

    :wrench: Impl

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 /// <reference path="https://raw.githubusercontent.com/proudust/deno-gas-types/main/types/index.d.ts" />
 
+import {
+  difference,
+  Unit,
+} from "https://deno.land/std@0.155.0/datetime/mod.ts";
+import { sortBy } from "https://deno.land/std@0.155.0/collections/sort_by.ts";
+
 declare let global: {
   doGet: (e?: GoogleAppsScript.Events.DoGet) =>
     | GoogleAppsScript.HTML.HtmlOutput
@@ -10,7 +16,79 @@ declare let global: {
   [key: string]: () => void;
 };
 
-global.createNewSpreadsheet = () => {
-  const spreadsheet = SpreadsheetApp.create("New file");
-  spreadsheet.getRange("A1").setValue("Hello World!");
+interface Options {
+  [labelName: string]:
+    | {
+      target: "date";
+      unit: Unit;
+      diff: number;
+    }
+    | {
+      target: "duplicates";
+      count: number;
+    };
+}
+
+function trashIfDateLater(
+  label: GoogleAppsScript.Gmail.GmailLabel,
+  unit: Unit,
+  diff: number,
+): void {
+  label
+    .getThreads()
+    .filter((thread) =>
+      diff <= (difference(
+        new Date(),
+        thread.getLastMessageDate() as Date,
+      )[unit] || Number.MIN_VALUE)
+    )
+    .forEach((thread) => {
+      console.log(
+        `Trash: ${thread.getFirstMessageSubject()} (${thread.getLastMessageDate().toISOString()})`,
+      );
+      return thread.moveToTrash();
+    });
+}
+
+function trashIfDuplicates(
+  label: GoogleAppsScript.Gmail.GmailLabel,
+  count: number,
+) {
+  sortBy(
+    label.getThreads(),
+    (thread) => thread.getLastMessageDate().getMilliseconds(),
+  )
+    .slice(count)
+    .forEach((thread) => {
+      console.log(
+        `Trash: ${thread.getFirstMessageSubject()} (${thread.getLastMessageDate().toISOString()})`,
+      );
+      return thread.moveToTrash();
+    });
+}
+
+global.gmailAutoDelete = () => {
+  const options: Options = JSON.parse(
+    PropertiesService
+      .getScriptProperties()
+      .getProperty("OPTIONS") || "{}",
+  );
+
+  Object.keys(options).forEach((labelName) => {
+    const label = GmailApp.getUserLabelByName(labelName);
+    const option = options[labelName];
+
+    switch (option.target) {
+      case "date": {
+        const { unit, diff } = option;
+        trashIfDateLater(label, unit, diff);
+        return;
+      }
+      case "duplicates": {
+        const { count } = option;
+        trashIfDuplicates(label, count);
+        return;
+      }
+    }
+  });
 };
